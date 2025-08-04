7553b2064dada15683c629ad67d9e9dd7a41b579
Dason Tsoi <dasontsoi@gmail.com>
2025-08-04 16:22:55 +0100

    updated version

--- app.py
+++ app.py
@@ -22,13 +22,9 @@ max_tokens = st.slider("Max tokens to generate:", 200, 1000, 500)
 if st.button("Generate"):
     if not prompt:
         prompt = "\n"
-    # Encode prompt
+    
     encoded = torch.tensor([encode(prompt)], dtype=torch.long, device=device)
-
-        # Generate tokens from prompt
     generated_ids = model.generate(encoded, max_new_tokens=max_tokens)[0].tolist()
-
-        # Decode tokens to string
     generated_text = decode(generated_ids)
 
     st.text_area("Generated Text", value=generated_text, height=300)
